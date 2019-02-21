83751132cd901cd524a75026a882ebe8bbf35ec9
JimmyHarrington <jimmyharrington@JimmyHarringtons-iMac.local>
2019-02-21 13:57:33 +0900

    Initial files

---
+++ Pods/Target Support Files/Pods-RealmMania4/Pods-RealmMania4-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RealmMania4 : NSObject
+@end
+@implementation PodsDummy_Pods_RealmMania4
+@end
---
+++ RealmMania4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RealmMania4
+//
+//  Created by JimmyHarrington on 2019/02/21.
+//  Copyright © 2019 JimmyHarrington. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
